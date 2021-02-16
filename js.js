3aedc4144f8c8d21d001af280de49bc37cb7db6b
Toivo <toivoh07@gmail.com>
2021-02-16 21:41:35 +0300

    16.02

--- js.js
+++ js.js
@@ -1,6 +1,17 @@
 function each(arr, callback) {
   let result = [];
-  for(let elem of art){
-    result.push(callback())
+  for(let elem of arr){
+    result.push(callback(elem));
   }
+  return result;
 }
+function strBack(str){
+  let arr = str.split('');
+  return arr.reverse().join('');
+}
+function getFirst(str){
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+let arr = ['abc','abc','abc'];
+console.log(each(arr,getFirst));
