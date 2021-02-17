ab1a1a621f438e33f9bab6f1663e648d7149d97d
Toivo <toivoh07@gmail.com>
2021-02-17 10:28:02 +0300

    callback

--- js.js
+++ js.js
@@ -1,10 +1,13 @@
 function each(arr, callback) {
   let result = [];
+  let i = 0;
   for(let elem of arr){
-    result.push(callback(elem));
+    result.push(callback(elem, i++));
   }
   return result;
 }
+
+
 function strBack(str){
   let arr = str.split('');
   return arr.reverse().join('');
@@ -12,6 +15,9 @@ function strBack(str){
 function getFirst(str){
   return str[0].toUpperCase() + str.slice(1);
 }
+function getMultiplication(elem, i) {
+     return elem * i;
+}
 
-let arr = ['abc','abc','abc'];
-console.log(each(arr,getFirst));
+let arr = [2,4,5,6,7,8,9];
+console.log(each(arr,getMultiplication));
