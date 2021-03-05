6f1722f55eb92e92f74b7b0e6761d757c35dd390
Toivo <toivoh07@gmail.com>
2021-03-05 12:59:47 +0300

    destruct,rest

--- js.js
+++ js.js
@@ -1,12 +1,80 @@
-function getSum(...rest) {
-   let sum = 0;
-   let count = 0;
-   for (const elem of rest) {
-      sum += elem;
-      count++;
-   }
-   return sum / count ;
+function getInt(...arr) {
+   
+}
+
+function inArrays(elem , arrs) {
+   
+}
+
+function inArray(arr,num) {
+   return arr.indexOf(num) != -1;
 }
 
 
-console.log(getSum(1,2,3,4,5,6,7));
+
+//ДЕСТРУКТУРИЗАЦИЯ
+
+// let arr = ['Иван', 'Иванов', 'отдел разработки', 'программист', 2000];
+
+// let [name ,surname ,department,position,salary] = arr;
+// console.log(name ,surname ,department,position,salary);
+
+// function func() {
+// 	return ['Иван', 'Иванов', 'отдел разработки'
+// 		, 'программист', 2000]; 
+// }
+// let [name ,surname ,department,position,salary] = func();
+// console.log(name ,surname ,department,position,salary);
+
+// let arr = ['Иван', 'Иванов', 'отдел разработки'
+// 	, 'программист', 2000]; 
+// let [,,department,position,] = arr;
+// console.log(department,position);
+
+// let arr = ['Иван', 'Иванов', 'отдел разработки'
+// 	, 'программист', 2000]; 
+
+//    let [name, surname, ...info] = arr;
+//    console.log(name,surname, info);
+
+// let arr = ['Иван', 'Иванов', 'отдел разработки'];
+// let [name, surname, department , position = 'джуниор' ] = arr;
+// console.log(name, surname, department , position);
+
+// let arr = [];
+// function func1() {
+//    return (new Date).getFullYear();
+// }
+// function func2() {
+//    return (new Date).getMonth();
+// }
+// function func3() {
+//    return (new Date).getDay();
+// }
+
+// let [year = func1(), month = func2(), day = func3()] = arr;
+// console.log(year,month,day);
+
+
+// let options = {
+// 	color: 'red',
+// 	width:  400,
+// 	height: 500,
+// };
+// let {color , width , height} = options;
+// console.log(color,width , height);
+
+// function func(department,[name,surname], hired ) {
+//     let arr = hired.split('-');
+//     let[year,month,day] = arr
+//    console.log(department,name,surname,year,month,day );
+// }
+
+// func( 'отдел разработки', ['Иван', 'Иванов'], '2018-12-31' );
+
+
+// function func({color = 'black',width,height}) {
+// 	console.log(color,width,height);
+// }
+
+// func( { width: 400, height: 500} );
